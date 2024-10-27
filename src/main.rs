c64cc828e5b90fecb7d64d3d0353bd5de9390206
Craig Hill <craigthomashill@gmail.com>
2024-10-27 16:01:22 +0000

    replace with simpler main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
-mod binding_and_mutability;
-mod scope;
-mod shadowing;
-mod unused_variables;
-
 fn main() {
-    binding_and_mutability::initialize();
-    binding_and_mutability::mutable();
-    scope::scope();
-    scope::define();
-    shadowing::shadow();
-    shadowing::rebind();
-    unused_variables::unused_variable();
-    println!("Success!");
+    println!("Hello, world!");
 }
