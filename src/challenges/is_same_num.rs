11813c4ee4330543045f51db5b842fec02563c86
Craig Hill <craigthomashill@gmail.com>
2024-10-27 16:53:24 +0000

    add challenge : is_same_num

---
+++ src/challenges/is_same_num.rs
@@ -0,0 +1,39 @@
+pub fn is_same_num(a: i32, b: i32) -> bool {
+    // TODO: Implement this function to make the tests pass
+    todo!() // This will cause the tests to fail until you implement it
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(is_same_num(4, 8), false, "4 and 8 should not be equal");
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(is_same_num(2, 2), true, "2 and 2 should be equal");
+    }
+
+    #[test]
+    fn test3() {
+        assert_eq!(is_same_num(0, 8), false, "0 and 8 should not be equal");
+    }
+
+    #[test]
+    fn test4() {
+        assert_eq!(is_same_num(1, 1), true, "1 and 1 should be equal");
+    }
+
+    #[test]
+    fn test5() {
+        assert_eq!(is_same_num(12, 3), false, "12 and 3 should not be equal");
+    }
+
+    #[test]
+    fn test6() {
+        assert_eq!(is_same_num(5, 98), false, "5 and 98 should not be equal");
+    }
+}
--- src/challenges/mod.rs
+++ src/challenges/mod.rs
@@ -3,5 +3,6 @@ pub mod add_one;
 pub mod addition;
 pub mod circuit_power;
 pub mod convert_minutes;
+pub mod is_same_num;
 pub mod return_true;
 pub mod squared;
