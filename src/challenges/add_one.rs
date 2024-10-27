ddca6a1cbfca464f4f833bf00f64315591370ab3
Craig Hill <craigthomashill@gmail.com>
2024-10-27 16:24:01 +0000

    add challenge: add_one

---
+++ src/challenges/add_one.rs
@@ -0,0 +1,34 @@
+pub fn addition(num: i32) -> i32 {
+    // TODO: Implement this function to make the tests pass
+    todo!() // This will cause the tests to fail until you implement it
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(addition(2), 3, "Should be 3");
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(addition(-9), -8, "Should be -8");
+    }
+
+    #[test]
+    fn test3() {
+        assert_eq!(addition(0), 1, "Should be 1");
+    }
+
+    #[test]
+    fn test4() {
+        assert_eq!(addition(999), 1000, "Should be 1000");
+    }
+
+    #[test]
+    fn test5() {
+        assert_eq!(addition(73), 74, "Should be 74");
+    }
+}
--- src/challenges/mod.rs
+++ src/challenges/mod.rs
@@ -1,3 +1,4 @@
 // This file declares the submodules
+pub mod add_one;
 pub mod addition;
 pub mod return_true;
