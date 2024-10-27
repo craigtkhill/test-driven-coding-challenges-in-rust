7609ef66ccd8ff7c2cabb36c475828a44559551b
Craig Hill <craigthomashill@gmail.com>
2024-10-27 16:49:32 +0000

    add challenge : convert_minutes

---
+++ src/challenges/convert_minutes.rs
@@ -0,0 +1,29 @@
+pub fn convert(minutes: i32) -> i32 {
+    // TODO: Implement this function to make the tests pass
+    todo!() // This will cause the tests to fail until you implement it
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(convert(6), 360, "Should convert 6 minutes to seconds");
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(convert(4), 240, "Should convert 4 minutes to seconds");
+    }
+
+    #[test]
+    fn test3() {
+        assert_eq!(convert(8), 480, "Should convert 8 minutes to seconds");
+    }
+
+    #[test]
+    fn test4() {
+        assert_eq!(convert(60), 3600, "Should convert 60 minutes to seconds");
+    }
+}
--- src/challenges/mod.rs
+++ src/challenges/mod.rs
@@ -2,5 +2,6 @@
 pub mod add_one;
 pub mod addition;
 pub mod circuit_power;
+pub mod convert_minutes;
 pub mod return_true;
 pub mod squared;
