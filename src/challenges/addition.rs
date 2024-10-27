19145a03e9461086b78eb54b0797299aed82b9b5
Craig Hill <craigthomashill@gmail.com>
2024-10-27 16:11:51 +0000

    add addition.rs challenge

---
+++ src/challenges/addition.rs
@@ -0,0 +1,29 @@
+// src/challenges/addition.rs
+pub fn addition(a: i32, b: i32) -> i32 {
+    // TODO: Implement this function to make the tests pass
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(addition(2, 3), 5, "Should return 5 for inputs 2 and 3");
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(
+            addition(-3, -6),
+            -9,
+            "Should return -9 for inputs -3 and -6"
+        );
+    }
+
+    #[test]
+    fn test3() {
+        assert_eq!(addition(7, 3), 10, "Should return 10 for inputs 7 and 3");
+    }
+}
