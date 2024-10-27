8a8e447f6b6b263781b90e0763b2976a42a480d7
Craig Hill <craigthomashill@gmail.com>
2024-10-27 17:05:56 +0000

    add challenge : less than or equal to zero

---
+++ src/challenges/less_than_or_equal_to_zero.rs
@@ -0,0 +1,72 @@
+pub fn less_than_or_equal_to_zero(num: i32) -> bool {
+    // TODO: Implement this function to make the tests pass
+    todo!() // This will cause the tests to fail until you implement it
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(
+            less_than_or_equal_to_zero(5),
+            false,
+            "Should return false for 5"
+        );
+    }
+
+    #[test]
+    fn test2() {
+        assert_eq!(
+            less_than_or_equal_to_zero(0),
+            true,
+            "Should return true for 0"
+        );
+    }
+
+    #[test]
+    fn test3() {
+        assert_eq!(
+            less_than_or_equal_to_zero(-5),
+            true,
+            "Should return true for -5"
+        );
+    }
+
+    #[test]
+    fn test4() {
+        assert_eq!(
+            less_than_or_equal_to_zero(1),
+            false,
+            "Should return false for 1"
+        );
+    }
+
+    #[test]
+    fn test5() {
+        assert_eq!(
+            less_than_or_equal_to_zero(2),
+            false,
+            "Should return false for 2"
+        );
+    }
+
+    #[test]
+    fn test6() {
+        assert_eq!(
+            less_than_or_equal_to_zero(10000),
+            false,
+            "Should return false for 10000"
+        );
+    }
+
+    #[test]
+    fn test7() {
+        assert_eq!(
+            less_than_or_equal_to_zero(1),
+            false,
+            "Should return false for 1"
+        );
+    }
+}
--- src/challenges/mod.rs
+++ src/challenges/mod.rs
@@ -4,6 +4,7 @@ pub mod addition;
 pub mod circuit_power;
 pub mod convert_minutes;
 pub mod is_same_num;
+pub mod less_than_or_equal_to_zero;
 pub mod next_edge;
 pub mod return_true;
 pub mod squared;
